f16c81fd41047ecbfa5bfd60db8abdd2926d488e
yas <yury.sukhovsy@yandex.ru>
2024-02-21 14:48:31 +0300

    cleanp in scopee of HadwareDiagnosticr

--- lights_gpio/src/lights_gpio.c
+++ lights_gpio/src/lights_gpio.c
@@ -59,44 +59,22 @@ static struct traffic_light_IMode *CreateIModeImpl(rtl_uint32_t step) {
 int main(void) {
     NkKosTransport transport;
     ServiceId iid;
-
-    /* Get lights gpio IPC handle of "lights_gpio_connection". */
     Handle handle = ServiceLocatorRegister("mc_gpio_connection", NULL, 0, &iid);
     assert(handle != INVALID_HANDLE);
-
-    /* Initialize transport to control system. */
     NkKosTransport_Init(&transport, handle, NK_NULL, 0);
-
-    /**
-     * Prepare the structures of the request to the lights gpio entity: constant
-     * part and arena. Because none of the methods of the lights gpio entity has
-     * sequence type arguments, only constant parts of the
-     * request and response are used. Arenas are effectively unused. However,
-     * valid arenas of the request and response must be passed to
-     * lights gpio transport methods (nk_transport_recv, nk_transport_reply) and
-     * to the lights gpio method.
-     */
     traffic_light_LightsGPIO_entity_req req;
     char req_buffer[traffic_light_LightsGPIO_entity_req_arena_size];
     struct nk_arena req_arena = NK_ARENA_INITIALIZER(req_buffer, req_buffer + sizeof(req_buffer));
-
-    /* Prepare response structures: constant part and arena. */
     traffic_light_LightsGPIO_entity_res res;
     char res_buffer[traffic_light_LightsGPIO_entity_res_arena_size];
     struct nk_arena res_arena = NK_ARENA_INITIALIZER(res_buffer, res_buffer + sizeof(res_buffer));
-
-    /**
-     * Initialize mode component dispatcher. 3 is the value of the step,
-     * which is the number by which the input value is increased.
-     */
     traffic_light_CMode_component component;
     traffic_light_CMode_component_init(&component, CreateIModeImpl(0x1000000));
 
-    /* Initialize lights gpio entity dispatcher. */
     traffic_light_LightsGPIO_entity entity;
     traffic_light_LightsGPIO_entity_init(&entity, &component);
+    fprintf(stderr, "[LightsGPIO   ] LightsGPIO service transport OK\n");
 
-    // HardwareDiagnostic connection init [START]
     NkKosTransport hd_transport;
     struct traffic_light_IDiagMessage_proxy hd_proxy;
     Handle hd_handle = ServiceLocatorConnect("gpio_diag_connection");
@@ -113,8 +91,7 @@ int main(void) {
     traffic_light_IDiagMessage_proxy_init(&hd_proxy, &hd_transport.base, hd_riid);
     traffic_light_IDiagMessage_Write_req hd_req;
     traffic_light_IDiagMessage_Write_res hd_res;
-
-    // HardwareDiagnostic connection init [END]
+    fprintf(stderr, "[LightsGPIO   ] HardwareDiagnostic client transport OK\n");
 
     fprintf(stderr, "[LightsGPIO   ] OK\n");
 
@@ -127,21 +104,14 @@ int main(void) {
     rtl_memset(ctl4, 0, 128);
 
 
-    /* Dispatch loop implementation. */
     do {
-        /* Flush request/response buffers. */
         nk_req_reset(&req);
         nk_arena_reset(&req_arena);
         nk_arena_reset(&res_arena);
 
-        /* Wait for request to lights gpio entity. */
         if (nk_transport_recv(&transport.base, &req.base_, &req_arena) != NK_EOK) {
             fprintf(stderr, "[LightsGPIO   ] nk_transport_recv error\n");
         } else {
-            /**
-             * Handle received request by calling implementation Mode_impl
-             * of the requested Mode interface method.
-             */
             format_traffic_lights(((u_int8_t*)&req.lightsGpio_mode.FMode.value)[0], (char *)&bs1);
             format_traffic_lights(((u_int8_t*)&req.lightsGpio_mode.FMode.value)[1], (char *)&bs2);
             format_traffic_lights(((u_int8_t*)&req.lightsGpio_mode.FMode.value)[2], (char *)&bs3);
@@ -167,7 +137,6 @@ int main(void) {
                      (char *)&bs1, (char *)&bs2, (char *)&bs3, (char *)&bs4, (rtl_uint32_t) req.lightsGpio_mode.FMode.value);
         send_diagnostic_message(&desc, (u_int32_t)rand(), buffer, "LightsGPIO");
 
-        /* Send response. */
         uint32_t resReply = nk_transport_reply(&transport.base, &res.base_, &res_arena);
         if (resReply != NK_EOK) {
             fprintf(stderr, "[LightsGPIO   ] nk_transport_reply error [%d]\n", resReply);
